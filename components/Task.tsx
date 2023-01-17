82d0fcacf138c38ce72a91b78ef9978f3e1551d0
YudaiTamura <y-tam@live.jp>
2023-01-17 22:39:46 +0900

    タスク詳細ページを作成

--- components/Task.tsx
+++ components/Task.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Task } from "../lib/tasks";
 
 export default function TaskComponent({ task }: { task: Task }) {
@@ -5,9 +6,11 @@ export default function TaskComponent({ task }: { task: Task }) {
     <div>
       <span>{task.id}</span>
       {" : "}
-      <span className="cursor-pointer text-white border-b border-gray-500 hover:bg-gray-600">
-        {task.title}
-      </span>
+      <Link href={`/tasks/${task.id}`}>
+        <span className="cursor-pointer text-white border-b border-gray-500 hover:bg-gray-600">
+          {task.title}
+        </span>
+      </Link>
     </div>
   );
 }
